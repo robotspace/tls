caf1b82135de96369a168f789c7f168e41f76ae5
robot <robotspace.biz@gmail.com>
2018-05-17 15:23:01 +0800

    Create keystore and truststore for Java.

--- ca.sh
+++ ca.sh
@@ -22,6 +22,26 @@ openssl x509 -req -days 36500 -sha1 -extensions v3_req -CA ../ca/ca-cert.pem -CA
 #使用CA证书验证server端证书
 openssl verify -CAfile ../ca/ca-cert.pem  server-cert.pem
 
+#生成服务器端的证书库
+keytool -genkey -keyalg RSA -alias endeca -keypass "123456" -keystore server-keystore.ks -storepass "123456" -dname "CN=name,OU=org,O=o,L=BJ,ST=BJ,C=CN"
+
+#删除其中的仓库信息
+keytool -delete -alias endeca -keystore server-keystore.ks -storepass "123456"
+
+#合并公钥和私钥
+cat server-cert.pem server-key.pem > combined.pem
+#转码为pkcs12
+openssl pkcs12 -export -out combined.pkcs12 -in combined.pem
+#导入公钥和私钥进入证书库
+keytool -v -importkeystore -srckeystore combined.pkcs12 -srcstoretype PKCS12 -destkeystore server-keystore.ks -deststoretype JKS
+
+#生成服务器端的信任证书库
+keytool -genkey -keyalg RSA -alias endeca -keypass "123456" -keystore server-truststore.ks -storepass "123456" -dname "CN=name,OU=org,O=o,L=BJ,ST=BJ,C=CN"
+#删除其中的仓库信息
+keytool -delete -alias endeca -keystore server-truststore.ks -storepass "123456"
+#导入CA（不需要导入对方公钥。双方基于共同的ＣＡ互相认可对方公钥？）
+keytool -import -v -trustcacerts -alias endeca-ca -file ../ca/ca-cert.pem -keystore server-truststore.ks
+exit 1
 cd ../client
 #生成客户端私钥
 openssl genrsa  -out client-key.pem 2048
@@ -32,5 +52,22 @@ openssl x509 -req -days 36500 -sha1 -extensions v3_req -CA  ../ca/ca-cert.pem -C
 #使用CA证书验证客户端证书
 openssl verify -CAfile ../ca/ca-cert.pem  client-cert.pem
 
+#生成客户端的证书库
+keytool -genkey -keyalg RSA -alias endeca -keypass "123456" -keystore client-keystore.ks -storepass "123456" -dname "CN=name,OU=org,O=o,L=BJ,ST=BJ,C=CN"
 
+#删除其中的仓库信息
+keytool -delete -alias endeca -keystore client-keystore.ks -storepass "123456"
+#合并公钥和私钥
+cat client-cert.pem client-key.pem > combined.pem
+#转码为pkcs12
+openssl pkcs12 -export -out combinedCert.pkcs12 -in combined.pem
+#导入公钥和私钥进入证书库
+keytool -v -importkeystore -srckeystore combinedCert.pkcs12 -srcstoretype PKCS12 -destkeystore client-keystore.ks -deststoretype JKS
+
+#生成客户端的信任证书库
+keytool -genkey -keyalg RSA -alias endeca -keypass "123456" -keystore client-truststore.ks -storepass "123456" -dname "CN=name,OU=org,O=o,L=BJ,ST=BJ,C=CN"
+#删除其中的仓库信息
+keytool -delete -alias endeca -keystore client-truststore.ks -storepass "123456"
+#导入CA（不需要导入对方公钥。双方基于共同的ＣＡ互相认可对方公钥？）
+keytool -import -v -trustcacerts -alias endeca-ca -file ../ca/ca-cert.pem -keystore client-truststore.ks
 
